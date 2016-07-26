3e921eac82e6d5a6eb8113f588b754e3804d0c4c
Steven Halase <steven.halase@gmail.com>
2016-07-26 00:10:31 -0600

    Initial commit

--- vidly/App_Start/BundleConfig.cs
+++ vidly/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace vidly
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
--- vidly/App_Start/RouteConfig.cs
+++ vidly/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace vidly
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new { controller = "Movies", action = "ByReleaseDate" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
